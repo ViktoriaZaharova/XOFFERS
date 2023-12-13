9b1428f6555d1f01c087b6227a317f14db32459f
Виктория Захарова <zaharova181114@gmail.com>
2023-12-14 01:06:05 +0200

    1

--- js/common.js
+++ js/common.js
@@ -1,4 +1,16 @@
-// $('[name="phone"]').mask('+7 (999) 999-99-99');
+// animate scroll
+$('.go_to').click(function (e) {
+	e.preventDefault();
+	var scroll_el = $(this).attr('href');
+	if ($(scroll_el).length !== 0) {
+		$('html, body').animate({
+			scrollTop: $(scroll_el).offset().top
+		}, 500);
+	}
+	// $('.mobile-menu').fadeOut();
+	return false;
+});
+
 // mobile menu
 $('.btn-burger').on('click', function () {
 	$('.nav-menu').fadeToggle();
@@ -7,3 +19,11 @@ $('.btn-burger').on('click', function () {
 $('.nav-menu__close').on('click', function () {
 	$('.nav-menu').fadeOut();
 });
+
+$('.nav-menu__close').on('click', function () {
+	$('.nav-menu').fadeOut();
+});
+
+
+// animate css + wow js
+new WOW().init();
