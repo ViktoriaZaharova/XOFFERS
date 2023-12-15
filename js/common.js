a7abd9f20425041d0cbda0aa63fb3d129a50283e
Виктория Захарова <zaharova181114@gmail.com>
2023-12-15 07:52:28 +0200

    +

--- js/common.js
+++ js/common.js
@@ -1,29 +1,113 @@
 // animate scroll
-$('.go_to').click(function (e) {
-	e.preventDefault();
-	var scroll_el = $(this).attr('href');
-	if ($(scroll_el).length !== 0) {
-		$('html, body').animate({
-			scrollTop: $(scroll_el).offset().top
-		}, 500);
-	}
-	// $('.mobile-menu').fadeOut();
-	return false;
+$(document).ready(function () { //плавный скролл
+	$(".go_to").on("click", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top,
+			headerHeight = $('header').height();
+
+		$('body,html').animate({ scrollTop: top - headerHeight }, 500);
+	});
 });
 
 // mobile menu
 $('.btn-burger').on('click', function () {
-	$('.nav-menu').fadeToggle();
+	$('.mobile-menu').addClass('open');
 });
 
-$('.nav-menu__close').on('click', function () {
-	$('.nav-menu').fadeOut();
+$('.mobile-menu__close').on('click', function () {
+	$('.mobile-menu').removeClass('open');
 });
 
-$('.nav-menu__close').on('click', function () {
-	$('.nav-menu').fadeOut();
+// header fixed
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 200) {
+		$('.wrapper').addClass('fixed');
+	} else {
+		$('.wrapper').removeClass('fixed');
+	}
 });
 
+$(document).ready(function () {
+	var block_show = null;
 
+	function scrollTracking() {
+		var wt = $(window).scrollTop();
+		var wh = $(window).height();
+		var et = $('footer').offset().top;
+		var eh = $('footer').outerHeight();
+
+		if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)) {
+			if (block_show == null || block_show == false) {
+				$('.wrapper').addClass('no-fixed');
+			}
+			block_show = true;
+		} else {
+			if (block_show == null || block_show == true) {
+				$('.wrapper').removeClass('no-fixed');
+			}
+			block_show = false;
+		}
+	}
+
+	$(window).scroll(function () {
+		scrollTracking();
+	});
+
+	$(document).ready(function () {
+		scrollTracking();
+	});
+});
+
+$(document).ready(function () {
+// var block_show = null;
+
+// function scrollTracking() {
+// 	var wt = $(window).scrollTop();
+// 	var wh = $(window).height();
+// 	var et = $('.advantages').offset().top;
+// 	var eh = $('.advantages').outerHeight();
+
+// 	if (et >= wt && et + eh <= wh + wt) {
+// 		if (block_show == null || block_show == false) {
+// 			$('body').addClass('stop-scroll');
+// 		}
+// 		block_show = true;
+// 	}
+// 	else {
+// 		if (block_show == null || block_show == true) {
+// 			$('body').removeClass('stop-scroll');
+// 		}
+// 		block_show = false;
+// 	}
+// }
+
+// $(window).scroll(function () {
+// 	scrollTracking();
+// });
+
+// $(document).ready(function () {
+// 	scrollTracking();
+// });
+
+
+// $('.whom-slider').slick({
+// 	slidesToShow: 1,
+// 	fade: true,
+// 	arrows: false,
+// 	dots: false,
+// 	autoplay: true,
+// 	autoplaySpeed: 2000,
+// 	draggable: true,
+// 	swipeToSlide: true,
+// 	pauseOnHover: false
+// });
+
+// $(".whom-slider").on("afterChange", function (event) {
+// 	if ($(this).find('.slick-slide').last().hasClass('slick-current')) {
+// 		$('body').removeClass('stop-scroll');
+// 	}
+// });
+});
 // animate css + wow js
 new WOW().init();
